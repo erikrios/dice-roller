1a44d00b49dbd36cfadde9cafdbdedf250b44fc2
erikrios <erikriosetiawan49@gmail.com>
2019-12-14 19:22:02 +0700

    Add and update image resources

--- app/src/main/java/com/erikriosetiawan/diceroller/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/diceroller/MainActivity.kt
@@ -2,15 +2,14 @@ package com.erikriosetiawan.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var rollButton: Button
-    private lateinit var resultText: TextView
-    private lateinit var countUp: Button
+    private lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         rollButton = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-        resultText = findViewById(R.id.result_text)
-        countUp = findViewById(R.id.count_up_button)
-        countUp.setOnClickListener { countUp() }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     /**
@@ -29,18 +26,14 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
 //        Toast.makeText(this, "button is clicked", Toast.LENGTH_SHORT).show()
         val randomInt = Random.nextInt(6) + 1
-        resultText.text = randomInt.toString()
-    }
-
-    private fun countUp() {
-        if (resultText.text.toString().equals("Hello, World!", true))
-            resultText.text = "1"
-        else {
-            var resultInt = resultText.text.toString().toInt()
-            if (resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage.setImageResource(drawableResource)
     }
 }
